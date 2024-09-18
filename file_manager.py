225b9c9d2381bfd5afb901bb336c0e2854c96aaa
CoderDude2 <isaacboots1@gmail.com>
2024-09-18 10:48:33 -0700

    Added FileManager class. Cleaned up code and moved functionality into their respective classes.

--- file_manager.py
+++ file_manager.py
@@ -2,8 +2,7 @@ import os
 import re
 from enum import Enum
 import math
-import shutil
-import time
+import json
 from dataclasses import dataclass, field
 
 import gather_prg
@@ -28,12 +27,24 @@ class Issue:
 
     def __eq__(self, other):
         return (self.issue_type == other.issue_type)
+    
+    def serialize_json(self):
+        serialized_issue = self.__dict__
+        serialized_issue['issue_type'] = serialized_issue['issue_type'].value
+        return serialized_issue
+
+    @classmethod
+    def deserialize_json(cls, json_string):
+        issue_type = IssueType(json_string['issue_type'])
+        message = json_string['message']
+        return cls(issue_type, message)
 
 @dataclass
 class FileData:
     file_name:str = None
     location:str = None
-    file_stat:os.stat_result = None
+    file_inode:int = None
+    file_mtime:float = None
     case_type:str = None
     issues:list[Issue] = field(default_factory=list)
     part_length:float = 0
@@ -48,80 +59,128 @@ class FileData:
     def full_path(self):
         return os.path.join(self.location, self.file_name)
 
-file_hashmap = {}
+    @classmethod
+    def from_path(cls, path):
+        fileData = cls()
+        fileData.file_name = os.path.basename(path).lower()
 
-def get_file_data(path) -> FileData:
-    fileData = FileData()
-    fileData.file_name = os.path.basename(path).lower()
+        fileData.location = os.path.dirname(path)
+        stat_result = os.stat(path)
+        fileData.file_inode = stat_result.st_ino
+        fileData.file_mtime = stat_result.st_mtime
 
-    fileData.location = os.path.dirname(path)
-    fileData.file_stat = os.stat(path)
-
-    with open(path, 'r') as file:
-        first_line = file.readline()
-        contents = file.readlines()
-    
-    if 'ASC' in first_line:
-        fileData.case_type = 'ASC'
-    elif 'T-L' in first_line or 'TLCS' in first_line or 'TLOC' in first_line:
-        fileData.case_type = "TLOC"
-    elif 'AOT14' in first_line:
-        fileData.case_type = "AOT"
-    else:
-        fileData.case_type = "DS"
-
-    contains_subprogram_0 = False
-    contains_subprogram_1 = False
-    contains_subprogram_2 = False
-
-    for i, line in enumerate(contents):
-        if '$0' in line:
-            contains_subprogram_0 = True
+        with open(path, 'r') as file:
+            first_line = file.readline()
+            contents = file.readlines()
         
-        if '$1' in line:
-            contains_subprogram_1 = True
-        
-        if '$2' in line:
-            contains_subprogram_2 = True
-        
-        if "(PartLength)" in line:
-            fileData.part_length = float(contents[i+1].split(' ')[1])
+        if 'ASC' in first_line:
+            fileData.case_type = 'ASC'
+        elif 'T-L' in first_line or 'TLCS' in first_line or 'TLOC' in first_line:
+            fileData.case_type = "TLOC"
+        elif 'AOT14' in first_line:
+            fileData.case_type = "AOT"
+        else:
+            fileData.case_type = "DS"
 
-        if "T0100 (CUT-OFF)" in line:
-            fileData.cut_off = float(contents[i+2][4:])
+        contains_subprogram_0 = False
+        contains_subprogram_1 = False
+        contains_subprogram_2 = False
 
-    if not contains_subprogram_0: 
-        fileData.add_issue(IssueType.SUBPROGRAM_0_ERR, f'Missing Subprogram: $0')
-    if not contains_subprogram_1:
-        fileData.add_issue(IssueType.SUBPROGRAM_1_ERR, f'Missing Subprogram: $1')
-    if not contains_subprogram_2:
-        fileData.add_issue(IssueType.SUBPROGRAM_2_ERR, f'Missing Subprogram: $2')
-    
-    difference = round(math.fabs(fileData.part_length - fileData.cut_off), 4)
-    if difference > 0.01:
-        fileData.add_issue(IssueType.PART_LENGTH_ERR, f'PART-LENGTH and CUT-OFF differ by {difference}')
-
-    if not prg_regex.match(fileData.file_name):
-        fileData.add_issue(IssueType.INVALID_NAME_ERR, message=f'{fileData.file_name} name is invalid')
-
-    if fileData.file_name == "4001.prg" and fileData.case_type == "ASC":
-        fileData.add_issue(IssueType.INVALID_NAME_ERR, f'{fileData.file_name} name is invalid for ASC case.')
-
-    return fileData
-
-for root, dirs, files in os.walk(gather_prg.REMOTE_PRG_PATH):
-    if len(files) > 0 and "ALL" not in os.path.basename(root) and not asc_folder_regex.match(os.path.basename(root)):
-        for name in files:
-            data = get_file_data(os.path.join(root, name))
-
-            if file_hashmap.get(name):
-                file_hashmap[name].append(data)
-            else:
-                file_hashmap[name] = [data]
+        for i, line in enumerate(contents):
+            if '$0' in line:
+                contains_subprogram_0 = True
             
-            print(data)
+            if '$1' in line:
+                contains_subprogram_1 = True
+            
+            if '$2' in line:
+                contains_subprogram_2 = True
+            
+            if "(PartLength)" in line:
+                fileData.part_length = float(contents[i+1].split(' ')[1])
 
-# for key in file_hashmap:
-#     print(key, file_hashmap[key])
+            if "T0100 (CUT-OFF)" in line:
+                fileData.cut_off = float(contents[i+2][4:])
 
-# print(file_data(r"\\192.168.1.100\Trubox\####ERP_RM####\Y2024\M09\D13\1. CAM\3. NC files\Sulgi\4643.prg"))
+        if not contains_subprogram_0: 
+            fileData.add_issue(IssueType.SUBPROGRAM_0_ERR, f'Missing Subprogram: $0')
+        if not contains_subprogram_1:
+            fileData.add_issue(IssueType.SUBPROGRAM_1_ERR, f'Missing Subprogram: $1')
+        if not contains_subprogram_2:
+            fileData.add_issue(IssueType.SUBPROGRAM_2_ERR, f'Missing Subprogram: $2')
+        
+        difference = round(math.fabs(fileData.part_length - fileData.cut_off), 4)
+        if difference > 0.01:
+            fileData.add_issue(IssueType.PART_LENGTH_ERR, f'PART-LENGTH and CUT-OFF differ by {difference}')
+
+        if not prg_regex.match(fileData.file_name):
+            fileData.add_issue(IssueType.INVALID_NAME_ERR, message=f'{fileData.file_name} name is invalid')
+
+        if fileData.file_name == "4001.prg" and fileData.case_type == "ASC":
+            fileData.add_issue(IssueType.INVALID_NAME_ERR, f'{fileData.file_name} name is invalid for ASC case.')
+
+        return fileData
+
+    def serialize_json(self):
+        serialized_output = self.__dict__
+        serialized_output['issues'] = [issue.serialize_json() for issue in serialized_output['issues']]
+        return serialized_output
+
+    @classmethod
+    def deserialize_json(cls, json_string):
+        file_name = json_string['file_name']
+        location = json_string['location']
+        file_inode = json_string['file_inode']
+        file_mtime = json_string['file_mtime']
+        case_type = json_string['case_type']
+        issues = [Issue.deserialize_json(issue) for issue in json_string['issues']]
+        part_length = json_string['part_length']
+        cut_off = json_string['cut_off']
+        return cls(
+            file_name,
+            location,
+            file_inode,
+            file_mtime,
+            case_type,
+            issues,
+            part_length,
+            cut_off
+        )
+
+class FileManager:
+    def __init__(self):
+        self.file_hashmap = {}
+    
+    def scan_folder(self):
+        for root, _, files in os.walk(gather_prg.REMOTE_PRG_PATH):
+            if len(files) > 0 and "ALL" not in os.path.basename(root) and not asc_folder_regex.match(os.path.basename(root)):
+                for name in files:
+                    if not self.file_hashmap.get(name):
+                        data = FileData.from_path(os.path.join(root, name))
+                        self.file_hashmap[name] = data
+                    else:
+                        fd:FileData = self.file_hashmap[name]
+                        f_stat = os.stat(os.path.join(root, name))
+
+                        if fd.location == root:
+                            if f_stat.st_mtime != fd.file_mtime:
+                                new_data = FileData.from_path(os.path.join(root, name))
+                                self.file_hashmap[name] = new_data
+
+
+    def save(self):
+        serialized_output = self.__dict__
+        for key in serialized_output['file_hashmap'].keys():
+            serialized_output['file_hashmap'][key] = serialized_output['file_hashmap'][key].serialize_json()
+        json_string = json.dumps(serialized_output, indent=' ')
+        with open('data.json', 'w+') as file:
+            file.write(json_string)
+
+    def load(self):
+        with open('data.json', 'r') as file:
+            content = file.read()
+        
+        loaded_json = json.loads(content)
+        self.file_hashmap = loaded_json['file_hashmap']
+        for i in self.file_hashmap.keys():
+            self.file_hashmap[i] = FileData.deserialize_json(self.file_hashmap[i])
